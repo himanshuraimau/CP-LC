97077121e66d61d98f0df49d2c571c893c3afa84
Himanshu Rai <himanshuraimau9@gmail.com>
2024-10-18 20:45:37 +0530

    add  2013A

---
+++ CF/2013A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+        x = min(x, y);
+        cout << (n + x - 1) / x << endl;
+    }
+}
